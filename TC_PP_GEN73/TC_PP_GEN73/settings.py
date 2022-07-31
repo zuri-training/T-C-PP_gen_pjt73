2341918b23e891f47def1409bbfa135d535b7cc7
kadelcode <kadelcode@gmail.com>
2022-07-31 09:33:00 +0100

    Initializing the project structure with models, templates, and static files configurations

--- TC_PP_GEN73/TC_PP_GEN73/settings.py
+++ TC_PP_GEN73/TC_PP_GEN73/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "TC_PP_GEN73/static"),
+    #'/var/www/static/',
+]
+
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
