bc81986ecf1e17982ef1f974b0c15cc7ff27eb91
ab <cruxCodes@gmail.com>
2022-08-06 23:24:27 +0100

    Urls

--- TC_PP_GEN73/blog/urls.py
+++ TC_PP_GEN73/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.homepage,name='index')
+    path('',views.homepage,name='index'),
+    path('/service',views.service,name='service'),
+
 ]
