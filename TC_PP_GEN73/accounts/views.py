41781598a1fa5b41ab64eb2ed7aecfeb10f78a4d
kadelcode <kadelcode@gmail.com>
2022-08-12 02:44:03 +0100

    Contact a lawyer logic with validation

--- TC_PP_GEN73/accounts/views.py
+++ TC_PP_GEN73/accounts/views.py
@@ -6,7 +6,10 @@ from django.contrib.auth import authenticate
 from django.contrib import messages
 from django.views.generic import View, CreateView, FormView
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, ContactLawyer
+from django.conf import settings
+from django.contrib import messages
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -54,8 +57,28 @@ def dashboard(request):
 def profile(request):
     return render(request, 'accounts/profile_page.html')
 
-def contact(request):
-    return render(request, 'accounts/lawyer.html')
+
+class ContactLawyerView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'accounts/lawyer.html')
+
+    def post(self,request, *args, **kwargs):
+        form = ContactLawyer(request.POST)
+        message = request.POST['message']
+        if form.is_valid():
+            send_mail('Contact Lawyer', message, settings.EMAIL_HOST_USER, ['kadelcode@gmail.com'], fail_silently=False)
+            messages.info(request,"Message successfully sent")
+            return redirect('contact_dashboard')
+        return render(request, 'accounts/lawyer.html', {'form':form})
+
+'''def contact(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+        send_mail('Need a Lawyer', message, settings.EMAIL_HOST_USER, ['kadelcode@gmail.com'], fail_silently=False)
+        messages.info(request,"Message successfully sent.")
+        return redirect('contact_dashboard')
+    else:
+        return render(request, 'accounts/lawyer.html')'''
 
 @login_required
 def history(request):
